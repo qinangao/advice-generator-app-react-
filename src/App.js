d062e0b93d90b1becce4cbd567ee3d186ec7536d
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-19 21:55:08 +0100

    hardcode jxs structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="app">
+      <p className="title">
+        ADVICE <span>#117</span>
+      </p>
+      <h2 className="advice">
+        "It's easyto sit up and take notice, what difficute is getting up and
+        taking action"
+      </h2>
+      <img src="/images/pattern-divider-desktop.svg" alt="divider" />
+      <button className="dice-btn">
+        <img src="/images/icon-dice.svg" alt="dice" />
+      </button>
+    </div>
+  );
+}
+
+export default App;
