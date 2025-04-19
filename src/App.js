cb1e0150c34572995d67026b497f2f94aaf7f201
qinanagao@gmail.com <qinanagao@gmail.com>
2025-04-19 23:06:37 +0100

    create app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  // https://api.adviceslip.com/advice(api)
+  const [advice, setAdvice] = useState({});
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 576);
+
+  async function handleGetAdvice() {
+    const res = await fetch(` https://api.adviceslip.com/advice`);
+    const data = await res.json();
+    setAdvice(data.slip);
+  }
+
+  useEffect(() => {
+    async function getAdvice() {
+      handleGetAdvice();
+    }
+    getAdvice();
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth < 576);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="app">
       <p className="title">
-        ADVICE <span>#117</span>
+        ADVICE <span>#{advice?.id}</span>
       </p>
-      <h2 className="advice">
-        "It's easyto sit up and take notice, what difficute is getting up and
-        taking action"
-      </h2>
-      <img src="/images/pattern-divider-desktop.svg" alt="divider" />
-      <button className="dice-btn">
+      <h2 className="advice">{advice?.advice}</h2>
+      <img
+        src={
+          isMobile
+            ? "/images/pattern-divider-mobile.svg"
+            : "/images/pattern-divider-desktop.svg"
+        }
+        alt="divider"
+      />
+      <button className="dice-btn" onClick={handleGetAdvice}>
         <img src="/images/icon-dice.svg" alt="dice" />
       </button>
     </div>
